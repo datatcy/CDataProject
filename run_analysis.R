a07392813cb648fefe50bc926c46cb3269e2b973
datatcy1 <fransipin@gmail.com>
2015-03-21 12:00:00 -0700

    projectcode and R code

---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+
+R version 3.1.2 (2014-10-31) -- "Pumpkin Helmet"
+Copyright (C) 2014 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[Previously saved workspace restored]
+
+> #1
+> tbl1 <- read.table("c://users/francispc/documents/train/x_train.txt")
+> tbl2 <- read.table("c://users/francispc/documents/test/x_test.txt")
+> A <- rbind(tbl1,tbl2)
+> 
+> tbl1 <- read.table("c://users/francispc/documents/train/subject_train.txt")
+> tbl2 <- read.table("c://users/francispc/documents/test/subject_test.txt")
+>  B <- rbind(tbl1,tbl2)
+> 
+>  tbl1 <- read.table("c://users/francispc/documents/train/y_train.txt")
+>  tbl2 <- read.table("c://users/francispc/documents/test/y_test.txt")
+>  C <- rbind(tbl1,tbl2)
+> 
+> #2
+> D <- read.table("c://users/francispc/documents/features.txt")
+>  good_features <- grep("-mean\\(\\)|-std\\(\\)",D[,2])
+>  A <-A[,good_features]
+>  names(A) <-D[good_features,2]
+>  names(A) <-gsub("\\(|\\)","",names(A))
+>  names(A) <-tolower(names(A))
+> 
+> #3
+>  act <- read.table("c://users/francispc/documents/activity_labels.txt")
+>  act[,2]= gsub("_","",tolower(as.character(act[,2])))
+>  C[,1] =act[C[,1],2]
+>  names(C) <- "activity"
+> 
+> #4
+>  names(B) <- "subject"
+>  cleaned <- cbind(B,C,A)
+>  write.table(cleaned, "merged_clean_data.txt")
+> 
